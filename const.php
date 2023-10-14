314835f78f0e6128ebfa5846102af7ec4c0f7fe3
Loduis Madariaga Barrios <loduis@gmail.com>
2023-10-14 11:18:57 -0500

    start: proyect

---
+++ const.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace FEEC;
+
+const ENV_TEST = 1;
+
+const ENV_PRO = 2;
+
+const TYPE_EMISSION = 1; // Es una constante revisar pagina 21 tabla 2
+
+const DOC_INVOICE = '01';
+
+const DOC_CREDIT_NOTE = '04';
+
+const STATUS_CREATED = 'CREADO';
+
+const STATUS_AUTHORIZED = 'AUTORIZADO';
+
+const STATUS_NOT_AUTHORIZED = 'NO AUTORIZADO';
+
+const STATUS_IN_PROCESSING = 'EN PROCESAMIENTO';
+
+const STATUS_REJECTED = 'RECHAZADO';
+
+const STATUS_BACK = 'DEVUELTA';
+
+const STATUS_RECEIVED = 'RECIBIDA';
