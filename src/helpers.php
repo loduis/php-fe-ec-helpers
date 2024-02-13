35a5a70cb223354dddfc8332aede41c9c48a96d4
Loduis Madariaga <loduis@gmail.com>
2024-02-13 10:34:05 -0500

    refactor: unifique helpers.

--- src/helpers.php
+++ src/helpers.php
@@ -60,3 +60,25 @@ const VAT_EXCLUDE = 6;
 const VAT_EXCEPT = 7;
 
 const VAT_DIFFERENTIATED = 8;
+
+
+function get_key(...$entries): string
+{
+
+    $key = implode('', $entries);
+
+    $sum = 0;
+    foreach (str_split(strrev($key)) as $i => $val) {
+        if ($i % 6 === 0) {
+            $factor = 2;
+        }
+        $sum += $val * $factor;
+        $factor ++;
+    }
+    $result = 11 - ($sum % 11);
+    if ($result > 9) {
+        $result = 11 - $result;
+    }
+
+    return $key . $result;
+}
